311ec5ee974bcb113b248718c6337813986f5e2c
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-01-23 12:36:50 +0100

    complete bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,3 +1,26 @@
 #include "sort.h"
+/**
+ * bubble_sort - sort using bubble sort algorithm
+ * @array: pointer to an array of int
+ * @size: size of the array
+ */
 
-
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+	
+	for (j = 0; j < (size - 1); j++)
+	{
+		for (i = 0; i < (size - 1); i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
