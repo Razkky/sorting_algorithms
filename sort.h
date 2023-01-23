f584d96cd773e8f40bff6cc29c930422bd790612
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-01-23 11:56:34 +0100

    header file

---
+++ sort.h
@@ -0,0 +1,9 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stido.h>
+#include <stdlib.h>
+#include <string.h>
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+#endif /* END SORT_H */
