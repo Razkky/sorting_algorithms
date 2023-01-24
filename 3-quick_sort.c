2eaa871e03404474c0ed5e5965cf8eb566e1eec3
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-01-25 00:54:16 +0100

    fix print only wen swapped

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -16,6 +16,7 @@ void quick_sort(int *array, size_t size)
  * @array: size of array
  * @low: begining of array
  * @high: ending of array
+ * @size: size of array
  */
 
 void quick_sort_recursion(int *array, int low, int high, size_t size)
@@ -23,6 +24,7 @@ void quick_sort_recursion(int *array, int low, int high, size_t size)
 	if (low < high)
 	{
 		int pivot_index = partition(array, low, high, size);
+
 		quick_sort_recursion(array, low, (pivot_index - 1), size);
 		quick_sort_recursion(array, (pivot_index + 1), high, size);
 	}
@@ -33,13 +35,14 @@ void quick_sort_recursion(int *array, int low, int high, size_t size)
  * @array: pointer to an array
  * @low: begining of array
  * @high: ending of array
+ * @size: size of array
  * Return: index of pivot element
  */
 
 int partition(int *array, int low, int high, size_t size)
 {
 	int i, j, pivot_element;
-	
+
 	i = low;
 	pivot_element = array[high];
 
@@ -47,13 +50,11 @@ int partition(int *array, int low, int high, size_t size)
 	{
 		if (array[j] < pivot_element)
 		{
-			swap_array(&array[j], &array[i]);
-			print_array(array, size);
+			swap_array(&array[j], &array[i], array, size);
 			i++;
 		}
 	}
-	swap_array(&array[i], &array[high]);
-	print_array(array, size);
+	swap_array(&array[i], &array[high], array, size);
 	return (i);
 }
 
@@ -61,13 +62,18 @@ int partition(int *array, int low, int high, size_t size)
  * swap_array - swap integers of an array
  * @i: pointer to one integer
  * @j: pointer to second
+ * @array: pointer to array of integers
+ * @size: size of array
  */
 
-void swap_array(int *i, int *j)
+void swap_array(int *i, int *j, int *array, size_t size)
 {
-	int tmp;
+	if (*i != *j)
+	{
+		int tmp = *j;
 
-	tmp = *i;
-	*i = *j;
-	*j = tmp;
+		*j = *i;
+		*i = tmp;
+		print_array(array, size);
+	}
 }
