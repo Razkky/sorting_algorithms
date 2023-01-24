6e93283c4a3b16e6f054ca910753bc0cd88aa194
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-01-24 08:05:44 +0100

    fix betty error

--- sort.h
+++ sort.h
@@ -12,9 +12,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
